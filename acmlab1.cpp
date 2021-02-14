ff16313db9dfdc5516882d4390bd2b4243e203ab
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2021-02-14 13:19:07 +0200

    Update acmlab1.cpp

--- acmlab1.cpp
+++ acmlab1.cpp
@@ -7,8 +7,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N1  18
-#define N2  27
+#define DATA_TYPE volatile unsigned long long int 
+
+#define N1 18
+#define N2 27
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
@@ -16,8 +18,6 @@
 #define REPEAT_COUNT 1000000
 #define REPEATOR(count, code) \
 for (unsigned int indexIteration = (count); indexIteration--;){ code; }
-#define TWO_VALUES_SELECTOR(variable, firstValue, secondValue) \
-	(variable) = indexIteration % 2 ? (firstValue) : (secondValue);
 
 double getCurrentTime(){
 	clock_t time = clock();
@@ -27,10 +27,10 @@ double getCurrentTime(){
 	return 0.; // else 	
 }
 
-unsigned long long int f1_GCD(unsigned long long int variableN1, unsigned long long int variableN2){
-    unsigned long long int returnValue = 1;	
+DATA_TYPE f1_GCD(DATA_TYPE variableN1, DATA_TYPE variableN2){
+	DATA_TYPE returnValue = 1;
 
-	for(unsigned long long int i = 1, k = MIN(variableN1, variableN2); i <= k; i++){	
+	for(DATA_TYPE i = 1, k = MIN(variableN1, variableN2); i <= k; i++){
 	    if(!(variableN1 % i || variableN2 % i)){
 	        returnValue = i;
 	    }	
@@ -39,8 +39,8 @@ unsigned long long int f1_GCD(unsigned long long int variableN1, unsigned long l
 	return returnValue;		
 }
 
-unsigned long long int f2_GCD(unsigned long long int a, unsigned long long int b){
-	for(unsigned long long int aModB; 
+DATA_TYPE f2_GCD(DATA_TYPE a, DATA_TYPE b){
+	for(DATA_TYPE aModB;
 		aModB = a % b, 
 		a = b, 
 		b = aModB;
@@ -49,7 +49,7 @@ unsigned long long int f2_GCD(unsigned long long int a, unsigned long long int b
 	return a;
 }
 
-unsigned long long int f3_GCD(unsigned long long int a, unsigned long long int b){
+DATA_TYPE f3_GCD(DATA_TYPE a, DATA_TYPE b){
     if(!b){
 	    return a;
 	}
@@ -58,19 +58,13 @@ unsigned long long int f3_GCD(unsigned long long int a, unsigned long long int b
 }
 
 int main() {
-    unsigned long long int vN1 = N1, vN2 = N2, a = MAX(vN1, vN2), b = MIN(vN1, vN2), 
-	vN1_ = vN1, vN2_ = vN2, a_ = a, b_ = b,
-	returnValue;
+	DATA_TYPE a = MAX(N1, N2), b = MIN(N1, N2),	returnValue;
 
 	double startTime, endTime;
 
 	// f1_GCD 
 	startTime = getCurrentTime();
-	REPEATOR(REPEAT_COUNT, 
-	    TWO_VALUES_SELECTOR(vN1, 16, vN1_); 
-	    TWO_VALUES_SELECTOR(vN2, 4, vN2_);
-	    returnValue = f1_GCD(a, b);
-	)	
+	REPEATOR(REPEAT_COUNT, returnValue = f1_GCD(a, b););
 	endTime = getCurrentTime();
 	printf("f1_GCD return %lld \r\nrun time: %dns\r\n\r\n", 
 	returnValue, 
@@ -78,11 +72,7 @@ int main() {
 
 	// f2_GCD 
 	startTime = getCurrentTime();
-	REPEATOR(REPEAT_COUNT, 
-	    TWO_VALUES_SELECTOR(a, 16, a_); 
-	    TWO_VALUES_SELECTOR(b, 4, b_);
-	    returnValue = f2_GCD(a, b);
-	)	
+	REPEATOR(REPEAT_COUNT, returnValue = f2_GCD(a, b););
 	endTime = getCurrentTime();
 	printf("f2_GCD return %lld \r\nrun time: %dns\r\n\r\n", 
 	returnValue, 
@@ -90,18 +80,14 @@ int main() {
 	
 	// f3_GCD 
 	startTime = getCurrentTime();
-	REPEATOR(REPEAT_COUNT, 
-	    TWO_VALUES_SELECTOR(a, 16, a_); 
-	    TWO_VALUES_SELECTOR(b, 4, b_);
-	    returnValue = f3_GCD(a, b);
-	)	
+	REPEATOR(REPEAT_COUNT, returnValue = f3_GCD(a, b););
 	endTime = getCurrentTime();
 	printf("f3_GCD return %lld \r\nrun time: %dns\r\n\r\n", 
 	returnValue,
 	(unsigned int)((endTime - startTime) * (double)(1000000000 / REPEAT_COUNT)));
 
-	printf("Press any key to continue . . .");
-	getchar();
+	printf("Press Enter to continue . . .");
+	(void)getchar();
 	
 	return 0;
 }
